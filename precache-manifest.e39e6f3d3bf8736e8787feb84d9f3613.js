74d6472952ed378378f11823a6fb82934d62e79e
Aram <kirakosyan.wb@gmail.com>
2020-04-03 13:59:54 +0400

    Updates

---
+++ precache-manifest.e39e6f3d3bf8736e8787feb84d9f3613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3186d467db698476a5e5a43f0b2a85b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2a62b3c249462217c983",
+    "url": "/monsters-rolodex/static/css/main.c4f22f1c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a62b3c249462217c983",
+    "url": "/monsters-rolodex/static/js/main.d0697f69.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
